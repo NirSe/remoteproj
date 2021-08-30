5dd24f912f08d56c3017c09b0bf35351e6e92c73
Nir Sela <33286680+NirSe@users.noreply.github.com>
2021-08-30 15:09:43 +0300

    new version

--- load.py
+++ load.py
@@ -5,6 +5,6 @@ from my_data import find_car
 def load_data(car: str = 'Toyota'):
     print ('in loading function')
 #    my_file_handle = open("/Users/nir/remtst/data.txt", 'w')
-    my_file_handle = open("/v3io/users/admin/nir/data.txt", 'w')
+    my_file_handle = open("/v3io/users/nir/remote/data.txt", 'w')
     car = find_car()
     my_file_handle.write(car)
