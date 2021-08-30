a20d8c00cf8f8ca325e7126b868d400c0fa44969
nirse <nirse@iguazio.com>
2021-08-30 14:05:53 +0000

    change name to 1

--- folder1/load1.py
+++ folder1/load1.py
@@ -6,5 +6,5 @@ def load_data1(car: str = 'Toyota'):
     print ('in loading function')
 #    my_file_handle = open("/Users/nir/remtst/data.txt", 'w')
     my_file_handle = open("/v3io/users/nir/remote/data.txt", 'w')
-    car = find_car()
+    car = find_car1()
     my_file_handle.write(car)
