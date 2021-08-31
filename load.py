97278f28b7ebebc8f342ae7aae507486e4a626b6
nir <nirse@iguazio.com>
2021-08-31 11:05:35 +0300

    write to local file

--- load.py
+++ load.py
@@ -3,8 +3,8 @@ from my_data import find_car
 
 
 def load_data(car: str = 'Toyota'):
-    print ('in loading function')
+    print('in loading function')
 #    my_file_handle = open("/Users/nir/remtst/data.txt", 'w')
-    my_file_handle = open("/v3io/users/nir/remote/data.txt", 'w')
+    my_file_handle = open("data.txt", 'w')
     car = find_car()
     my_file_handle.write(car)
